58b105d494b151e18d8a33c5b5e4f57319da43c0
Axcelion017 <michaelnewton1708@gmail.com>
2026-03-31 14:41:37 +0700

    Pretest

--- PreTest/src/ClassMahasiswa.java
+++ PreTest/src/ClassMahasiswa.java
@@ -7,10 +7,4 @@ class ClassMahasiswa{
         this.umur = umur;
         this.jurusan = jurusan;
     }
-
-    void display(){
-        System.out.println("Nama: " + nama);
-        System.out.println("Umur: " + umur);
-        System.out.println("Jurusan: " + jurusan);
-    }
 }
--- PreTest/src/MainMahasiswa.java
+++ PreTest/src/MainMahasiswa.java
@@ -1,7 +1,5 @@
 public class MainMahasiswa{
     public static void main(String[] args) throws Exception {
         ClassMahasiswa mhsBaru = new ClassMahasiswa("Andi", 20, "Informatika");
-        mhsBaru.display();
     }
-
 }
